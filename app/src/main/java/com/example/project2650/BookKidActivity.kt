8d903044068685f87b92aaa3da7b5f4f07de5c6a
amikom <pamel4515@gmail.com>
2023-10-04 14:52:26 +0700

    add fragment update

---
+++ app/src/main/java/com/example/project2650/BookKidActivity.kt
@@ -0,0 +1,41 @@
+package com.example.project2650
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class BookKidActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_kid)
+
+        //INSTANCE
+        val btnFairy:Button = findViewById(R.id.buttonFairy)
+        val btnFable:Button = findViewById(R.id.buttonFable)
+        val btnScience:Button = findViewById(R.id.buttonScience)
+
+        //event button fairy
+        btnFairy.setOnClickListener {
+            replaceFragment(FairyFragment())
+        }
+
+        //event button fable
+        btnFable.setOnClickListener {
+            replaceFragment(FableFragment())
+        }
+
+        //event button science
+        btnScience.setOnClickListener {
+            replaceFragment(ScienceFragment())
+        }
+    }
+
+    //create method replace fragment
+    private fun replaceFragment (frg:Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTrx = fragmentManager.beginTransaction()
+        fragmentTrx.replace(R.id.fragmentKidBook,frg)
+        fragmentTrx.commit()
+    }
+}
--- app/src/main/java/com/example/project2650/HomeActivity.kt
+++ app/src/main/java/com/example/project2650/HomeActivity.kt
@@ -2,10 +2,19 @@ package com.example.project2650
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //instance
+        val btnBookKid: Button = findViewById(R.id.buttonBookKid)
+
+        //event button book kid
+        btnBookKid.setOnClickListener{
+        }
     }
 }
